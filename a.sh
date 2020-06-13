ebf56db054472dad5cf292e90b59846d868b39d0
poonamdesle <55239953+poonamdesle@users.noreply.github.com>
2020-06-14 00:38:10 +0530

    sf

--- a.sh
+++ a.sh
@@ -1,1 +1,11 @@
 echo "HI"
+
+uname=$(jq -r '.uname' authinfo.json | basr64 -d)
+pwd=$(jq -r '.pwd' authinfo.json | basr64 -d)
+team=trial
+client_id=$(jq -r '.client_id' authinfo.json | basr64 -d)
+secret=$(jq -r '.secret' authinfo.json | basr64 -d)
+
+rm -rf "$team" && mkdir "$team" && cd $team
+curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > authinfo.json
+jq -r '.access_token' authinfo.json > auth.txt
