d10ec6fabb32a8188cace66b082c729272bb4785
poonamdesle <55239953+poonamdesle@users.noreply.github.com>
2020-06-14 00:29:00 +0530

    as

--- gitrepo.sh
+++ gitrepo.sh
@@ -2,9 +2,9 @@
 
 uname=$(jq -r '.uname' authinfo.json | base64 -d)
 pwd=$(jq -r '.pwd' authinfo.json | base64 -d)
-team=energycomponent
 client_id=$(jq -r '.client_id' authinfo.json | base64 -d)
 secret=$(jq -r '.secret' authinfo.json | base64 -d)
+team=trial
 
 rm -rf "$team" && mkdir "$team" && cd $team
 curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > authinfo.json
