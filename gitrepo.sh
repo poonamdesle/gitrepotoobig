75f46b40bb5fb9c11feccfa2aca35ae35975d8d4
poonamdesle <55239953+poonamdesle@users.noreply.github.com>
2020-06-13 23:56:20 +0530

    creat gitrepo.sh

---
+++ gitrepo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+uname=$(jq -r '.uname' authinfo.json | basr64 -d)
+pwd=$(jq -r '.pwd' authinfo.json | basr64 -d)
+team=energycomponent
+client_id=$(jq -r '.client_id' authinfo.json | basr64 -d)
+secret=$(jq -r '.secret' authinfo.json | basr64 -d)
+
+rm -rf "$team" && mkdir "$team" && cd $team
+curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > authinfo.json
+jq -r '.access_token' authinfo.json > auth.txt
+
+NEXT_URL="https://api.bitbucket.org/2.0/repositories/$team/$USERNAME?access_token=$access-token"
+
+    curl -L -X POST -u "$client_id:$secret" $URL > authinfo.json
+	jq -r '.access_token' authinfo.json > auth.txt
+	for i in $(cat auth.txt);
+	do
+         access-token=$(echo $i | awk -F '{ print $1}' )
+		 
+	done	 
+    
+	curl -u $USERNAME:$PWD $NEXT_URL > repoinfo.json
+    jq -r '.values[] | .name' repoinfo.json > repos.txt
+    
+    for i in $(cat repos.txt);
+    do
+        
+		reponame=$(echo $i | awk -F '{ print $1}' )
+		repourl= git@bitbucket.org:$uname/$reponame.git
+		git clone $repourl
+        echo "$(du -shc $repo_name/*)"
+        echo "$(du -shc $repo_name/*)" | awk 'NR==2{print $1}'
+        size=$(echo "$(du -shc $repo_name/*)" | awk 'NR==2{print $1}')
+        echo "Total size  of repository is $size"
+        if [ $size > $threshold ]
+        then 
+            echo " Requided to cleanup of repository"
+        fi
+		
+    done
+	
