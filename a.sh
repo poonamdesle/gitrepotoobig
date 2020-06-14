f8b0977448adf1385709a492757a49206ded8d51
poonamdesle <55239953+poonamdesle@users.noreply.github.com>
2020-06-14 14:11:27 +0530

    as

--- a.sh
+++ a.sh
@@ -7,5 +7,5 @@ client_id=$(jq -r '.client_id' authinfo.json | basr64 -d)
 secret=$(jq -r '.secret' authinfo.json | basr64 -d)
 
 rm -rf "$team" && mkdir "$team" && cd $team
-curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > authinfo.json
-jq -r '.access_token' authinfo.json > auth.txt
+curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > /var/lib/jenkins/workspace/aaru/$team/authinfo.json
+jq -r '.access_token' /var/lib/jenkins/workspace/aaru/$team/authinfo.jsonauthinfo.json > /var/lib/jenkins/workspace/aaru/$team/authinfo.jsonauth.txt
