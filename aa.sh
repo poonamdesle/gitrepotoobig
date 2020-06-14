23675c234b524d3499d8295178d6adfbe5b3a8e1
poonamdesle <55239953+poonamdesle@users.noreply.github.com>
2020-06-14 14:43:16 +0530

    sad

--- aa.sh
+++ aa.sh
@@ -1,13 +1,16 @@
-
 #!/bin/bash
 
-uname=$(jq -r '.uname' authinfo.json | base64 -d)
-pwd=$(jq -r '.pwd' authinfo.json | base64 -d)
+uname=$(jq -r '.uname' config.json | base64 -d)
+echo "$uname"
+pwd=$(jq -r '.pwd' config.json | base64 -d)
+echo "$pwd"
 team=trial
-client_id=$(jq -r '.client_id' authinfo.json | base64 -d)
-secret=$(jq -r '.secret' authinfo.json | base64 -d)
-
+client_id=$(jq -r '.client_id' config.json | base64 -d)
+echo "$client_id"
+secret=$(jq -r '.secret' config.json | base64 -d)
+echo "$secret"
 rm -rf "$team" && mkdir "$team" && cd $team
+
 curl -L -X POST -u "$client_id:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=password -d username=$uname -d password=$pwd > authinfo.json
 jq -r '.access_token' authinfo.json > auth.txt
 accesstoken="$(cat auth.txt)"
